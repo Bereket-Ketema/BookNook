cb01d30277fa80bcd3c2bf595b727a29714ad222
Bereket-Ketema <bereketketema866@gmail.com>
2026-08-13 17:04:35 +0300

    the page build provider and builder have been added

---
+++ lib/features/library/presentation/pages/book_page.dart
@@ -0,0 +1,57 @@
+import 'package:book_nook/core/injection/injection_container.dart';
+import 'package:book_nook/features/library/domain/usecases/get_books.dart';
+import 'package:book_nook/features/library/presentation/cubit/book_cubit.dart';
+import 'package:book_nook/features/library/presentation/cubit/book_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BookPage extends StatelessWidget {
+  const BookPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => BookCubit(sl<GetBooksUseCase>()),
+      child: const BookView(),
+    );
+  }
+}
+
+class BookView extends StatelessWidget {
+  const BookView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Book')),
+      body: BlocBuilder<BookCubit, BookState>(
+        builder: (context, state) {
+          if (state is BookInitial) {
+            return Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  context.read<BookCubit>().loadBooks();
+                },
+                child: const Text('Load Books'),
+              ),
+            );
+          }
+
+          if (state is BookLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (state is BookLoaded) {
+            return Center(child: Text(state.books.toString()));
+          }
+
+          if (state is BookError) {
+            return Center(child: Text(state.message));
+          }
+
+          return const SizedBox();
+        },
+      ),
+    );
+  }
+}
