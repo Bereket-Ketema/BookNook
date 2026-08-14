aacf15b37d6dcaa2cdef48343cf488d61a5c6605
Bereket-Ketema <bereketketema866@gmail.com>
2026-08-14 21:55:47 +0300

    the get book, update, delete, add use cases of the injection have been added

--- lib/features/library/presentation/pages/book_page.dart
+++ lib/features/library/presentation/pages/book_page.dart
@@ -1,10 +1,15 @@
-import 'package:book_nook/core/injection/injection_container.dart';
-import 'package:book_nook/features/library/domain/usecases/get_books.dart';
-import 'package:book_nook/features/library/presentation/cubit/book_cubit.dart';
-import 'package:book_nook/features/library/presentation/cubit/book_state.dart';
-import 'package:book_nook/features/library/presentation/widgets/book_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:book_nook/core/injection/injection_container.dart';
+
+import 'package:book_nook/features/library/domain/usecases/get_books.dart';
+import 'package:book_nook/features/library/domain/usecases/add_book.dart';
+import 'package:book_nook/features/library/domain/usecases/update_book.dart';
+import 'package:book_nook/features/library/domain/usecases/delete_book.dart';
+
+import '../cubit/book_cubit.dart';
+import 'book_view.dart';
 
 class BookPage extends StatelessWidget {
   const BookPage({super.key});
@@ -12,72 +17,13 @@ class BookPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => BookCubit(sl<GetBooksUseCase>()),
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Book Nook')),
-        body: const BookView(),
-      ),
+      create: (_) => BookCubit(
+        sl<GetBooksUseCase>(),
+        sl<AddBookUseCase>(),
+        sl<UpdateBookUseCase>(),
+        sl<DeleteBookUseCase>(),
+      )..loadBooks(),
+      child: const BookView(),
     );
   }
-}
-
-class BookView extends StatefulWidget {
-  const BookView({super.key});
-
-  @override
-  State<BookView> createState() => _BookViewState();
-}
-
-class _BookViewState extends State<BookView> {
-  @override
-  void initState() {
-    super.initState();
-
-    context.read<BookCubit>().loadBooks();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<BookCubit, BookState>(
-      //Build my UI using the state produced by BookCubit
-      builder: (context, state) {
-        if (state is BookInitial) {
-          return const Center(child: Text('Ready to load books'));
-        }
-
-        if (state is BookLoading) {
-          return const Center(child: CircularProgressIndicator());
-        }
-
-        if (state is BookLoaded) {
-          if (state.books.isEmpty) {
-            return const Center(
-              child: Text('No books found'),
-            );
-          }
-
-          return ListView.builder(
-            padding: const EdgeInsets.all(12),
-            itemCount: state.books.length,
-            itemBuilder: (context, index) {
-              final book = state.books[index];
-
-              return BookCard(
-                book: book,
-              );
-            },
-          );
-        }
-
-        if (state is BookError) {
-          return Center(
-            child: Text(state.message),
-          );
-        }
-
-        return SizedBox.shrink();
-      },
-    );
-  }
-
 }
