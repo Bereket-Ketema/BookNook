183ce45d33f9b3b669f144558f27d872a0353219
Bereket-Ketema <bereketketema866@gmail.com>
2026-08-14 22:01:21 +0300

    the get books use case have been updated

--- lib/features/library/domain/usecases/get_books.dart
+++ lib/features/library/domain/usecases/get_books.dart
@@ -8,7 +8,13 @@ class GetBooksUseCase {
 
   GetBooksUseCase(this.repository);
 
-  Future<Either<Failure, List<Book>>> call() {
-    return repository.getBooks();
+  Future<Either<Failure, List<Book>>> call() async {
+    print('USECASE: started');
+
+    final result = await repository.getBooks();
+
+    print('USECASE: repository returned');
+
+    return result;
   }
 }
