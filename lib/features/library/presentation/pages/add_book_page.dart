06a2e54c0eb261445a81d0b557b1e478957977e6
Bereket-Ketema <bereketketema866@gmail.com>
2026-08-14 21:53:31 +0300

    the add book page of the form have been added

--- lib/features/library/presentation/pages/add_book_page.dart
+++ lib/features/library/presentation/pages/add_book_page.dart
@@ -1,4 +1,8 @@
+import 'package:book_nook/features/library/domain/entities/book.dart';
+import 'package:book_nook/features/library/presentation/cubit/book_cubit.dart';
+import 'package:book_nook/features/library/presentation/cubit/book_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddBookPage extends StatefulWidget {
   const AddBookPage({super.key});
@@ -14,6 +18,7 @@ class _AddBookPageState extends State<AddBookPage> {
   final coverImageController = TextEditingController();
   final categoryController = TextEditingController();
   final publishedYearController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void dispose() {
@@ -30,10 +35,155 @@ class _AddBookPageState extends State<AddBookPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add Book'),
+      appBar: AppBar(title: const Text('Add Book')),
+
+      body: BlocListener<BookCubit, BookState>(
+        listener: (context, state) {
+          if (state is BookActionSuccess) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.message)));
+
+            Navigator.pop(context, true);
+          }
+
+          if (state is BookActionError) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.message)));
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: titleController,
+                      decoration: const InputDecoration(labelText: 'Title'),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter a title';
+                        }
+
+                        return null;
+                      },
+                    ),
+
+                    const SizedBox(height: 12),
+
+                    TextFormField(
+                      controller: authorController,
+                      decoration: const InputDecoration(labelText: 'Author'),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter an author';
+                        }
+
+                        return null;
+                      },
+                    ),
+
+                    const SizedBox(height: 12),
+
+                    TextFormField(
+                      controller: descriptionController,
+                      decoration: const InputDecoration(
+                        labelText: 'Description',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter a description';
+                        }
+
+                        return null;
+                      },
+                    ),
+
+                    const SizedBox(height: 12),
+
+                    TextFormField(
+                      controller: coverImageController,
+                      decoration: const InputDecoration(labelText: 'Image'),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter an Image';
+                        }
+
+                        return null;
+                      },
+                    ),
+
+                    const SizedBox(height: 12),
+
+                    TextFormField(
+                      controller: categoryController,
+                      decoration: const InputDecoration(labelText: 'Category'),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter a Category';
+                        }
+
+                        return null;
+                      },
+                    ),
+
+                    const SizedBox(height: 12),
+
+                    TextFormField(
+                      controller: publishedYearController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(labelText: 'Year'),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter the published year';
+                        }
+
+                        if (int.tryParse(value.trim()) == null) {
+                          return 'Please enter a valid year';
+                        }
+
+                        return null;
+                      },
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          final book = Book(
+                            id: DateTime.now().millisecondsSinceEpoch
+                                .toString(),
+                            title: titleController.text.trim(),
+                            author: authorController.text.trim(),
+                            description: descriptionController.text.trim(),
+                            coverImageUrl: coverImageController.text.trim(),
+                            category: categoryController.text.trim(),
+                            publishedYear: int.parse(
+                              publishedYearController.text.trim(),
+                            ),
+                            isFavorite: false,
+                            isRead: false,
+                          );
+
+                          context.read<BookCubit>().addBook(book);
+                        }
+                      },
+                      child: const Text('Add Book'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
-      body: ListBody(),
     );
   }
 }
