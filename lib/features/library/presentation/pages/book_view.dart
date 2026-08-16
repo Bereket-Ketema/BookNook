388015853ab8759c480b2984c133879d4e186bed
Bereket-Ketema <bereketketema866@gmail.com>
2026-08-16 12:16:17 +0300

    the edit part have been arranged

--- lib/features/library/presentation/pages/book_view.dart
+++ lib/features/library/presentation/pages/book_view.dart
@@ -16,7 +16,7 @@ class BookView extends StatelessWidget {
         title: const Text('Book Nook'),
       ),
 
-      body: BlocListener<BookCubit, BookState>(
+      body: BlocConsumer<BookCubit, BookState>(
         listener: (context, state) {
           if (state is BookActionSuccess) {
             ScaffoldMessenger.of(context).showSnackBar(
@@ -35,145 +35,124 @@ class BookView extends StatelessWidget {
           }
         },
 
-        child: BlocBuilder<BookCubit, BookState>(
-          builder: (context, state) {
+        builder: (context, state) {
+          // LOADING
+          if (state is BookLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
 
-            // LOADING
-            if (state is BookLoading) {
+          // ERROR
+          if (state is BookError) {
+            return Center(
+              child: Text(state.message),
+            );
+          }
+
+          // LOADED
+          if (state is BookLoaded) {
+            if (state.books.isEmpty) {
               return const Center(
-                child: CircularProgressIndicator(),
+                child: Text('No books found'),
               );
             }
 
-            // ERROR
-            if (state is BookError) {
-              return Center(
-                child: Text(state.message),
-              );
-            }
+            return ListView.builder(
+              itemCount: state.books.length,
+              itemBuilder: (context, index) {
+                final book = state.books[index];
 
-            // LOADED
-            if (state is BookLoaded) {
-
-              if (state.books.isEmpty) {
-                return const Center(
-                  child: Text('No books found'),
-                );
-              }
-
-              return ListView.builder(
-                itemCount: state.books.length,
-                itemBuilder: (context, index) {
-
-                  final book = state.books[index];
-
-                  return ListTile(
-                    title: Text(book.title),
-                    subtitle: Text(book.author),
-
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-
-                        // EDIT
-                        IconButton(
-                          icon: const Icon(Icons.edit),
-                          onPressed: () async {
-
-                            final result = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) => BlocProvider.value(
-                                  value: context.read<BookCubit>(),
-                                  child: EditBookPage(
-                                    book: book,
-                                  ),
+                return ListTile(
+                  title: Text(book.title),
+                  subtitle: Text(book.author),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // EDIT
+                      IconButton(
+                        icon: const Icon(Icons.edit),
+                        onPressed: () async {
+                          final result = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => BlocProvider.value(
+                                value: context.read<BookCubit>(),
+                                child: EditBookPage(
+                                  book: book,
                                 ),
                               ),
-                            );
+                            ),
+                          );
 
-                            if (result == true) {
-                              // ignore: use_build_context_synchronously
-                              context
-                                  .read<BookCubit>()
-                                  .loadBooks();
-                            }
-                          },
-                        ),
+                          if (result == true) {
+                            // ignore: use_build_context_synchronously
+                            context.read<BookCubit>().loadBooks();
+                          }
+                        },
+                      ),
 
-                        // DELETE
-                        IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () async {
-
-                            final confirmed =
-                                await showDialog<bool>(
-                              context: context,
-                              builder: (context) {
-
-                                return AlertDialog(
-                                  title: const Text(
-                                    'Delete Book',
-                                  ),
-
-                                  content: const Text(
-                                    'Are you sure you want to delete this book?',
-                                  ),
-
-                                  actions: [
-
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(
-                                          context,
-                                          false,
-                                        );
-                                      },
-                                      child: const Text(
-                                        'Cancel',
-                                      ),
+                      // DELETE
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () async {
+                          final confirmed = await showDialog<bool>(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text(
+                                  'Delete Book',
+                                ),
+                                content: const Text(
+                                  'Are you sure you want to delete this book?',
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(
+                                        context,
+                                        false,
+                                      );
+                                    },
+                                    child: const Text(
+                                      'Cancel',
                                     ),
-
-                                    ElevatedButton(
-                                      onPressed: () {
-                                        Navigator.pop(
-                                          context,
-                                          true,
-                                        );
-                                      },
-                                      child: const Text(
-                                        'Delete',
-                                      ),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pop(
+                                        context,
+                                        true,
+                                      );
+                                    },
+                                    child: const Text(
+                                      'Delete',
                                     ),
-                                  ],
-                                );
-                              },
-                            );
+                                  ),
+                                ],
+                              );
+                            },
+                          );
 
-                            if (confirmed == true) {
-                              // ignore: use_build_context_synchronously
-                              context
-                                  .read<BookCubit>()
-                                  .deleteBook(book.id);
-                            }
-                          },
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              );
-            }
+                          if (confirmed == true) {
+                            // ignore: use_build_context_synchronously
+                            context.read<BookCubit>().deleteBook(book.id);
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
 
-            return const SizedBox();
-          },
-        ),
+          return const SizedBox();
+        },
       ),
-
-      // ADD BOOK
+      
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-
           final result = await Navigator.push(
             context,
             MaterialPageRoute(
@@ -186,12 +165,9 @@ class BookView extends StatelessWidget {
 
           if (result == true) {
             // ignore: use_build_context_synchronously
-            context
-                .read<BookCubit>()
-                .loadBooks();
+            context.read<BookCubit>().loadBooks();
           }
         },
-
         child: const Icon(Icons.add),
       ),
     );
