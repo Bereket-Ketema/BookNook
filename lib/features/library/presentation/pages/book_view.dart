e103e282e4f99f3681436cf3d5f76ae8a65119d5
Bereket-Ketema <bereketketema866@gmail.com>
2026-08-17 08:25:47 +0300

    the import have been removed

--- lib/features/library/presentation/pages/book_view.dart
+++ lib/features/library/presentation/pages/book_view.dart
@@ -1,4 +1,3 @@
-import 'package:book_nook/features/library/presentation/pages/edit_book_page.dart';
 import 'package:book_nook/features/library/presentation/widgets/book_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
