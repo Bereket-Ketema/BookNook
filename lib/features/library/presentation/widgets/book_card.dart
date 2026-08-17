c484b46fd9b920aea8c2c6baf205183230402d2a
Bereket-Ketema <bereketketema866@gmail.com>
2026-08-17 08:55:36 +0300

    the edit and delete methods have been added

--- lib/features/library/presentation/widgets/book_card.dart
+++ lib/features/library/presentation/widgets/book_card.dart
@@ -1,12 +1,17 @@
-import 'package:flutter/material.dart';
+
 import 'package:book_nook/features/library/domain/entities/book.dart';
+import 'package:flutter/material.dart';
 
 class BookCard extends StatelessWidget {
   final Book book;
+  final VoidCallback onEdit;
+  final VoidCallback onDelete;
 
   const BookCard({
     super.key,
     required this.book,
+    required this.onEdit,
+    required this.onDelete,
   });
 
   @override
@@ -15,6 +20,19 @@ class BookCard extends StatelessWidget {
       child: ListTile(
         title: Text(book.title),
         subtitle: Text(book.author),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.edit),
+              onPressed: onEdit,
+            ),
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: onDelete,
+            ),
+          ],
+        ),
       ),
     );
   }
